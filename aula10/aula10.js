fc6e25000a81fbac1d92fc8f452e01d51374771e
ALAN2002JC <141837158+ALAN2002JC@users.noreply.github.com>
2023-09-03 18:26:41 -0300

    Add files via upload

---
+++ aula10/aula10.js
@@ -0,0 +1,7 @@
+const nome = prompt("qual seu nome?")
+const email = prompt('qual seu email?')
+
+const fraseTemplate = `o e-mail ${email} foi cadastrado com sucesso. Seja bem-vinda(o), ${nome}!`
+
+
+console.log(fraseTemplate)
