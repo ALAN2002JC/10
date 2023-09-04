82e80fdc35546f32b667d6b30d3b85966af7e599
ALAN2002JC <141837158+ALAN2002JC@users.noreply.github.com>
2023-09-03 21:47:26 -0300

    Add files via upload

---
+++ aula10p2/aula10p2.js
@@ -0,0 +1,14 @@
+//Desafio: pergunte ao usuário uma comida preferida. Troque a segunda comida da sua lista pela resposta do usuário e imprima no console a nova lista.
+ 
+const comidaspreferidas = ['estrogonofe', 'churrasco', 'massa bolonhesa', 'esfirra', 'pizza']
+console.log('essas são minhas comidas favoritas')
+
+console.log(comidaspreferidas)
+
+//
+
+comidaspreferidas.splice(1, 1)
+
+const comidaspreferidas = prompt('qual sua comida preferida?')
+
+
